d6429129895772877dd6c17ee780b2631040b2e2
Bautista Querejeta <41586620@ort>
2019-05-22 20:32:58 -0300

    formulario

---
+++ Scripts/script.js
@@ -0,0 +1,33 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    // Espero que se cargue el dom
+
+
+    document.getElementById("submit").addEventListener("click", function () {
+        //Chequeamo email
+        let inputemail = document.getElementById("inputE").innerText
+        var emailSucceeded = false;
+        if (inputemail == "" || inputemail.includes(" ")) {
+            event.preventDefault(); // Prevengo enviar
+            //muestro error
+            alert("Porfavor ingrese un mail valido");
+        } else {
+            emailSucceeded = true;
+
+        }
+
+        // Chequeamo password
+        let password = document.getElementById("inputPass").innerText
+        if (password.length > 8 && password.length < 12 && !password.includes(" ") && emailSucceeded) {
+            //success
+            alert("Muy bien, enviado correctamente!")
+        } else if (!emailSucceeded) { // Fallo alguna de mis validaciones de contraseña
+            //fail
+            alert("Porfavor ingrese una contraseña valida");
+        }
+    });
+
+
+});
+
+
+
